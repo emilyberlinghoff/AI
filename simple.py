11a2d68415a8ca287dae71bf4aa8679368ef6d9c
emilyberlinghoff <126015922+emilyberlinghoff@users.noreply.github.com>
2024-02-20 20:09:41 -0500

    Used the custom chatbot code

--- simple.py
+++ simple.py
@@ -1,6 +1,20 @@
 import openai
+import gradio
 
-openai.api_key = "####"
+openai.api_key = "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"
 
-completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Give me 3 ideas for apps I could build with openai apis "}])
-print(completion.choices[0].message.content)
+messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate Pro")
+
+demo.launch(share=True)
