075ef304c5778dc91b10adbcabbe53777fcc15e3
emilyberlinghoff <126015922+emilyberlinghoff@users.noreply.github.com>
2024-02-20 20:47:39 -0500

    migrate

--- simple.py
+++ simple.py
@@ -1,14 +1,15 @@
-import openai
+from openai import OpenAI
+client = OpenAI(
+  api_key=os.environ['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'],
+)
 import gradio
-
-openai.api_key = "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"
 
 messages = [{"role": "system", "content": "You are a math and programming expert"}]
 
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
+        model = "gpt-4-turbo",
         messages = messages
     )
     ChatGPT_reply = response["choices"][0]["message"]["content"]
