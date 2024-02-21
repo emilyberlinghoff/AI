9d102ce80e00c6f8710451fbed4e8eb498d2b202
emilyberlinghoff <126015922+emilyberlinghoff@users.noreply.github.com>
2024-02-20 20:26:27 -0500

    update name and model used

--- simple.py
+++ simple.py
@@ -3,18 +3,18 @@ import gradio
 
 openai.api_key = "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"
 
-messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+messages = [{"role": "system", "content": "You are a math and programming expert"}]
 
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
     response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
+        model = "gpt-4-turbo",
         messages = messages
     )
     ChatGPT_reply = response["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
 
-demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate Pro")
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Computer Science assistant")
 
 demo.launch(share=True)
